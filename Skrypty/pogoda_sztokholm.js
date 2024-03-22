206d5c80efc7febda18b5bacf5de9e8d1f4a2935
Eynemy <mateuszzimny27@gmail.com>
2024-03-22 23:51:50 +0100

    waluty 1.0

--- Skrypty/pogoda_sztokholm.js
+++ Skrypty/pogoda_sztokholm.js
@@ -8,23 +8,23 @@ fetch('https://api.open-meteo.com/v1/forecast?latitude=59.3293&longitude=18.0686
         var poziom_deszczu = "/TUR/Obrazy/";
         if (deszcz < 15){
             // poziom_deszczu +='<b>Bardzo lekki deszcz</b>'
-            poziom_deszczu +='bardzo lekki deszcz.png'
+            poziom_deszczu +='bardzo lekki deszcz.png" title="bardzo lekki deszcz" '
         }
         else if(deszcz < 24){
             // poziom_deszczu +='<b>Lekki deszcz</b>'
-            poziom_deszczu +='lekki deszcz.png'
+            poziom_deszczu +='lekki deszcz.png" title="lekki deszcz" '
         }
         else if (deszcz < 96){
             // poziom_deszczu +='<b>Umiarkowany deszcz</b>'
-            poziom_deszczu +='umiarkowany deszcz.png'
+            poziom_deszczu +='umiarkowany deszcz.png" title="umiarkowany deszcz" '
         }
         else if (deszcz < 288){
             // poziom_deszczu +='<b>Ulewa</b>'
-            poziom_deszczu +='ulewny1.png'
+            poziom_deszczu +='ulewny1.png" title="ulewa" '
         }
         else{
             // poziom_deszczu +='<b>Nawałnica</b>'
-            poziom_deszczu +='nawałnica.png'
+            poziom_deszczu +='nawałnica.png" title="nawałnica" '
         }
         
     
@@ -59,7 +59,7 @@ fetch('https://api.open-meteo.com/v1/forecast?latitude=59.3293&longitude=18.0686
         weatherCard.className = 'weather-card';
         weatherCard.innerHTML = `
             <h3>${Dzien}</h3>
-            <img src="${poziom_deszczu}" width="40px">
+            <img src="${poziom_deszczu} width="40px">
             <p>Temperatura max: <b style=" ${max_kolor};"> ${data.daily.temperature_2m_max[i]}°C</b></p>
             <p>Temperatura min: <b style=" ${min_kolor};"> ${data.daily.temperature_2m_min[i]}°C</b></p>
             <p>Opady: <b> ${data.daily.precipitation_sum[i]}mm</b></p>
